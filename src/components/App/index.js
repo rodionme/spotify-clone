fc836906a5dbb91668d17a1fcff6eb4e3f4a91bb
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-10 23:38:16 +0300

    Update `App` component with examples of `Button` and `Icon` components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
 import './style.css';
+import Button from '../Button';
+import Icon from '../Icon';
+import { ICONS } from '../../constants/icons';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <h1 className="app-title">Welcome to React</h1>
-      </div>
+      <React.Fragment>
+        <Button>Regular button</Button>
+
+        <Button view="rounded">
+          <Icon icon={ICONS.MORE} />
+        </Button>
+
+        <Button view="action">Action button</Button>
+
+        <Button view="action-alt">Action Alt button</Button>
+
+        <Button view="inverted">Inverted button</Button>
+
+        <Button view="alt">Alternative button</Button>
+
+        <Button view="transparent">
+          <Icon icon={ICONS.PLUS} />
+        </Button>
+
+        <br />
+        <br />
+
+        <Button type="link" link="/test">
+          Regular button
+        </Button>
+
+        <Button view="rounded" type="link" link="/test">
+          <Icon icon={ICONS.MORE} />
+        </Button>
+
+        <Button view="action" type="link" link="/test">
+          Action button
+        </Button>
+
+        <Button view="action-alt" type="link" link="/test">
+          Action Alt button
+        </Button>
+
+        <Button view="inverted" type="link" link="/test">
+          Inverted button
+        </Button>
+
+        <Button view="alt" type="link" link="/test">
+          Alternative button
+        </Button>
+
+        <Button view="transparent" type="link" link="/test">
+          <Icon icon={ICONS.PLUS} />
+        </Button>
+      </React.Fragment>
     );
   }
 }
