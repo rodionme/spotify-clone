52d1a8fbb50529d2dfa11599cefde321292a2e30
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-18 18:49:18 +0300

    Extract example data and add type checking for `ComplexList` component

--- src/components/ComplexList/index.js
+++ src/components/ComplexList/index.js
@@ -1,190 +1,71 @@
 import React, { Component } from 'react';
+import PropType from 'prop-types';
 import './style.css';
 import Section from '../ComplexListSection';
 
 export default class ComplexList extends Component {
   render() {
-    let complexList = {
-      albums: [
-        {
-          id: 1,
-          link: '',
-          thumb: {
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Mura Masa',
-            subtitle: 'Mura Masa',
-          },
-        },
-        {
-          id: 2,
-          link: '',
-          thumb: {
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Multi-Love',
-            subtitle: 'Unknown Mortal Orchestra',
-          },
-        },
-        {
-          id: 3,
-          link: '',
-          thumb: {
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Mutter',
-            subtitle: 'Rammstein',
-          },
-        },
-      ],
-      artists: [
-        {
-          id: 1,
-          link: '',
-          thumb: {
-            isRounded: true,
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Muse',
-          },
-        },
-        {
-          id: 2,
-          link: '',
-          thumb: {
-            isRounded: true,
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Mura Masa',
-          },
-        },
-        {
-          id: 3,
-          link: '',
-          thumb: {
-            isRounded: true,
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Muddy Waters',
-          },
-        },
-      ],
-      bestMatch: [
-        {
-          id: 1,
-          link: '',
-          thumb: {
-            isRounded: true,
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Muse',
-          },
-        },
-      ],
-      playlists: [
-        {
-          id: 1,
-          link: '',
-          thumb: {
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Music For Concentration',
-          },
-        },
-        {
-          id: 2,
-          link: '',
-          thumb: {
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Musicals Hits',
-          },
-        },
-        {
-          id: 3,
-          link: '',
-          thumb: {
-            img: '//via.placeholder.com/100x100',
-          },
-          content: {
-            title: 'Music for Malling',
-          },
-        },
-      ],
-      tracks: [
-        {
-          id: 1,
-          link: '',
-          content: {
-            title: 'Multi-Love',
-            subtitle: 'Unknown Mortal Orchestra',
-          },
-        },
-        {
-          id: 2,
-          link: '',
-          content: {
-            title: 'Mudshovel - Explicit Album Version',
-            subtitle: 'Staind',
-          },
-        },
-        {
-          id: 3,
-          link: '',
-          content: {
-            title: 'Musicbox',
-            subtitle: 'Amy Yeager',
-          },
-        },
-      ],
-    };
-
+    let { bestMatch, tracks, artists, albums, playlists } = this.props.list;
     return (
       <div className="complex-list">
         <Section
           section={{
             title: 'Top Result',
-            list: complexList.bestMatch,
+            list: bestMatch,
           }}
         />
 
         <Section
           section={{
             title: 'Songs',
-            list: complexList.tracks,
+            list: tracks,
           }}
         />
 
         <Section
           section={{
             title: 'Artists',
-            list: complexList.artists,
+            list: artists,
           }}
         />
 
         <Section
           section={{
             title: 'Albums',
-            list: complexList.albums,
+            list: albums,
           }}
         />
 
         <Section
           section={{
             title: 'Playlists',
-            list: complexList.playlists,
+            list: playlists,
           }}
         />
       </div>
     );
   }
 }
+
+const listSection = PropType.shape({
+  id: PropType.number.isRequired,
+  link: PropType.string.isRequired,
+  thumb: PropType.shape({
+    isRounded: PropType.bool,
+    img: PropType.string,
+  }),
+  content: PropType.shape({
+    title: PropType.string.isRequired,
+    subtitle: PropType.string,
+  }),
+});
+
+ComplexList.propTypes = {
+  list: PropType.shape({
+    bestMatch: PropType.arrayOf(listSection),
+    tracks: PropType.arrayOf(listSection),
+    artists: PropType.arrayOf(listSection),
+    albums: PropType.arrayOf(listSection),
+    playlists: PropType.arrayOf(listSection),
+  }),
+};
--- src/components/demo/complex-lists.js
+++ src/components/demo/complex-lists.js
@@ -3,9 +3,153 @@ import ComplexList from '../ComplexList';
 
 export default class ComplexLists extends Component {
   render() {
+    let complexList = {
+      albums: [
+        {
+          id: 1,
+          link: '',
+          thumb: {
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Mura Masa',
+            subtitle: 'Mura Masa',
+          },
+        },
+        {
+          id: 2,
+          link: '',
+          thumb: {
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Multi-Love',
+            subtitle: 'Unknown Mortal Orchestra',
+          },
+        },
+        {
+          id: 3,
+          link: '',
+          thumb: {
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Mutter',
+            subtitle: 'Rammstein',
+          },
+        },
+      ],
+      artists: [
+        {
+          id: 1,
+          link: '',
+          thumb: {
+            isRounded: true,
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Muse',
+          },
+        },
+        {
+          id: 2,
+          link: '',
+          thumb: {
+            isRounded: true,
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Mura Masa',
+          },
+        },
+        {
+          id: 3,
+          link: '',
+          thumb: {
+            isRounded: true,
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Muddy Waters',
+          },
+        },
+      ],
+      bestMatch: [
+        {
+          id: 1,
+          link: '',
+          thumb: {
+            isRounded: true,
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Muse',
+          },
+        },
+      ],
+      playlists: [
+        {
+          id: 1,
+          link: '',
+          thumb: {
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Music For Concentration',
+          },
+        },
+        {
+          id: 2,
+          link: '',
+          thumb: {
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Musicals Hits',
+          },
+        },
+        {
+          id: 3,
+          link: '',
+          thumb: {
+            img: '//via.placeholder.com/100x100',
+          },
+          content: {
+            title: 'Music for Malling',
+          },
+        },
+      ],
+      tracks: [
+        {
+          id: 1,
+          link: '',
+          content: {
+            title: 'Multi-Love',
+            subtitle: 'Unknown Mortal Orchestra',
+          },
+        },
+        {
+          id: 2,
+          link: '',
+          content: {
+            title: 'Mudshovel - Explicit Album Version',
+            subtitle: 'Staind',
+          },
+        },
+        {
+          id: 3,
+          link: '',
+          content: {
+            title: 'Musicbox',
+            subtitle: 'Amy Yeager',
+          },
+        },
+      ],
+    };
+
     return (
       <React.Fragment>
-        <ComplexList />
+        <ComplexList list={complexList} />
       </React.Fragment>
     );
   }
